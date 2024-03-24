2b9e14dbb18acd5fd53089dc3b80733c8db2f4a9
alessiomirra <alessiomirra97@gmail.com>
2024-03-24 07:53:27 +0100

    added sql file

--- database/structure.sql
+++ database/structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Mar 13, 2024 alle 08:59
+-- Creato il: Mar 24, 2024 alle 07:49
 -- Versione del server: 10.4.28-MariaDB
 -- Versione PHP: 8.2.4
 
@@ -24,6 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `project_id` int(10) UNSIGNED NOT NULL,
+  `path` varchar(70) NOT NULL,
+  `extension` varchar(10) NOT NULL,
+  `description` text DEFAULT NULL,
+  `timestamp` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `projects`
 --
 
@@ -38,18 +54,6 @@ CREATE TABLE `projects` (
   `status` enum('compleated','working') NOT NULL DEFAULT 'working'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `projects`
---
-
-INSERT INTO `projects` (`id`, `name`, `description`, `start`, `deadline`, `user_id`, `notes`, `status`) VALUES
-(1, 'Sample Project', 'Sample Project Description', '2024-03-07', '2025-04-09', 2, 'Sample project notes', 'working'),
-(3, 'Testing project form', 'Description project testing. Edit Test Passed', '2024-03-10', '2024-03-15', 1, 'Some additional notes', 'working'),
-(5, 'Second Redirect Test', 'Second Redirect Description', '2024-03-10', '2024-03-31', 2, 'Second Redirect Description             ', 'working'),
-(6, 'Third Redirect Test', 'Third Redirect Test Description', '2024-03-10', '2024-04-18', 2, 'Third Redirect Test Description           ', 'working'),
-(7, 'Test', 'Test description', '2024-03-10', '2024-04-26', 2, 'Test Notes', 'working'),
-(8, 'Test2', 'Test2 description', '2024-03-10', '2024-03-04', 4, 'Test2 notes', 'working'),
-(9, 'Test3', 'Test3 description', '2024-03-10', '2024-02-26', 4, 'Test3 Notes', 'working');
 
 -- --------------------------------------------------------
 
@@ -68,17 +72,6 @@ CREATE TABLE `tasks` (
   `project_id` int(4) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `tasks`
---
-
-INSERT INTO `tasks` (`id`, `name`, `description`, `creation`, `deadline`, `status`, `user_id`, `project_id`) VALUES
-(3, 'Edit Task Test', 'Second task description', '2024-03-11', '2024-03-28', 'working', 4, 1),
-(4, 'Task Test', 'Task Test description', '2024-03-11', '2024-04-21', 'working', 1, 8),
-(5, 'A task for that project', 'A description for that task', '2024-03-11', '2024-03-29', 'working', 1, 6),
-(6, 'Task 1', 'Task 1 description', '2024-03-12', '2024-03-29', 'working', 4, 3),
-(7, 'Task 2', 'Task 2 description', '2024-03-12', '2024-03-31', 'working', 1, 5),
-(8, 'Task 3', 'Task 3 description', '2024-03-12', '2024-04-16', 'working', 4, 7);
 
 -- --------------------------------------------------------
 
@@ -93,24 +86,21 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL,
   `roletype` enum('admin','visitor') NOT NULL DEFAULT 'visitor',
   `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL
+  `last_name` varchar(50) DEFAULT NULL,
+  `avatar` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `user`
---
-
-INSERT INTO `user` (`id`, `username`, `email`, `password`, `roletype`, `first_name`, `last_name`) VALUES
-(1, 'alessiomirra', 'alessio@gmail.com', '$2y$10$Zec6b1z5nyUQ5g1L8RUWfOtzm7uwxtQ/NKkqlQl74AUeAPf7HDhfS', 'visitor', 'Alessio', 'Mirra'),
-(2, 'admin', 'admin@php.com', '$2y$10$Zec6b1z5nyUQ5g1L8RUWfOtzm7uwxtQ/NKkqlQl74AUeAPf7HDhfS', 'admin', NULL, NULL),
-(4, 'tester', 'tester@gmail.com', '$2y$10$01DQcxgX/FGwBFRSqvVKweKYSRBkQkGjYtCOHNOzm82Upq2tkERG2', 'visitor', 'Giulio', 'Cesare'),
-(10, 'test', 'test@gmail.com', '$2y$10$V6yAFQDXXYvay0U1HvUhDeoZe66RP2Bpvsw4QQQvpb.Y4GzJ59WXG', 'visitor', 'Pinco', 'Pallo'),
-(12, 'test3', 'test3@gmail.com', '$2y$10$1TN5ojrG7LEkXVOmFjd8WuRke0YdFBrG6jkfIM/YeoJ7oEza.dgZ6', 'visitor', NULL, NULL),
-(13, 'test4', 'test4@gmail.com', '$2y$10$XLanH5cxC0jLnJwW9NHCdOW7DsS66P1dZMSr5qhnsHiE1pfas41TW', 'visitor', 'Marco', 'Mazzoli');
 
 --
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_file_project_id` (`project_id`),
+  ADD KEY `fk_file_user_id` (`user_id`);
 
 --
 -- Indici per le tabelle `projects`
@@ -138,6 +128,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT per la tabella `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT per la tabella `projects`
 --
 ALTER TABLE `projects`
@@ -158,6 +154,13 @@ ALTER TABLE `user`
 --
 -- Limiti per le tabelle scaricate
 --
+
+--
+-- Limiti per la tabella `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `fk_file_project_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_file_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `projects`
