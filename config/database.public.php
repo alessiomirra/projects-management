b2547c43ea755324daa3ed69c6da154ee312f201
alessiomirra <alessiomirra97@gmail.com>
2024-03-24 08:01:57 +0100

    configs

--- config/database.public.php
+++ config/database.public.php
@@ -1,11 +1,16 @@
 <?php 
 
+/**
+* This is a sample file to show how to config database connection
+* Link this file in public/index.php   
+*/
+
 return [
     'driver' => 'mysql', 
     'host' => 'localhost:3306', 
-    'user' => 'root', 
-    'password' => '26623881', 
-    'database' => 'project', 
+    'user' => 'databaseUser', 
+    'password' => 'password', 
+    'database' => 'databaseName', 
     'charset' => 'utf8',
     'pdooptions' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
